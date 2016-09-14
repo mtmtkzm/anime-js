485e414c39be0cd95d3ecb851cf82d30c95b639a
松本 和馬 <hello@mtmtkzm.com>
2016-09-15 02:04:40 +0900

    体裁を整えた

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-/* DEMO2
+/* DEMO1
  *************** */
 anime({
   targets: '.circle',
@@ -11,53 +11,3 @@ anime({
   duration: 1000,
   loop: true
 });
-
-/* DEMO2
- *************** */
-var timeline = new mojs.Timeline();
-var circle = document.querySelector('#mo-toggle'),
-    parent = document.querySelector('#demo2');
-
-burstTween = new mojs.Burst({
-    parent: parent,
-    duration: 2500,
-    shape: 'circle',
-    fill: 'orange',
-    x: '50%',
-    y: '50%',
-    count: 12,
-    isRunLess: true, // trueにするとload時に発火しない
-    easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
-}),
-fadeTween = new mojs.Tween({
-	duration : 300,
-	onUpdate: function(progress) {
-		circle.style.transform = 'scale(' + (1 - progress*0.6) + ')';
-	}
-});
-
-timeline.add(burstTween, fadeTween);
-
-circle.addEventListener('click', function() {
-    timeline.start();
-    console.log(timeline);
-})
-
-
-
-/* DEMO1
- *************** */
-
-function init() {
-    var square = document.querySelector('.steam');
-
-    new mojs.Tween({
-        repeat: 100,
-        duration: 2000,
-        onUpdate: function(progress) {
-            square.style.transform = 'translateX(' + 3 * Math.sin(2 * progress * Math.PI) + 'px)';
-        }
-    }).run();
-}
-
-init();
