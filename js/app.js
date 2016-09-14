1d139f6e551927602e9eaf0151ae672298e39a3d
松本 和馬 <hello@mtmtkzm.com>
2016-09-15 03:18:02 +0900

    普通の円形ローディングの配置だけできた

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
+/* DEMO2
+ *************** */
+var circleLength = 12;
+var r = 100;
+var container = document.querySelector('#container');
+
+for (var i=0; i<circleLength; i++) {
+  var div = document.createElement('div');
+  var dx = r * Math.sin(Math.PI*2/circleLength * i);
+  var dy = r * Math.cos(Math.PI*2/circleLength * i);
+  div.style.transform = 'translate(' + dx + 'px, ' + dy + 'px)';
+  div.classList.add('circle2');
+  container.appendChild(div);
+}
+
+
+
 /* DEMO1
  *************** */
 anime({
-  targets: '.circle',
+  targets: '.circle1',
   translateY: '30px',
   delay: function(el, index) {
     return index * 160;
