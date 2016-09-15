1f8b8a8d8bf8ce27e5e88d20569e41c747cefe02
松本 和馬 <hello@mtmtkzm.com>
2016-09-15 16:58:09 +0900

    中央から外へ吐き出されるアニメーション

--- js/app.js
+++ js/app.js
@@ -1,23 +1,38 @@
 /* DEMO2
  *************** */
 var circleLength = 12;
-var r = 100;
+var r = 70;
 var container = document.querySelector('#container');
 
 for (var i=0; i<circleLength; i++) {
   var div = document.createElement('div');
-  var dx = r * Math.sin(Math.PI*2/circleLength * i);
-  var dy = r * Math.cos(Math.PI*2/circleLength * i);
-  div.style.transform = 'translate(' + dx + 'px, ' + dy + 'px)';
   div.classList.add('circle2');
   container.appendChild(div);
 }
 
+var simpleLoadAnime = anime({
+  targets: '.circle2',
+  scale: [1, 1],
+  translateX: function(element, index) {
+    var dx = Math.floor(r * Math.sin(Math.PI*2/circleLength * index));
+    return dx + 'px';
+  },
+  translateY: function(element, index) {
+    var dy = Math.floor(r * Math.cos(Math.PI*2/circleLength * index));
+    return dy + 'px';
+  },
+  delay: function(element, index) {
+    return index * 100;
+  },
+  elasticity: 200,
+  duration: 1000,
+  loop: true
+});
 
 
 /* DEMO1
  *************** */
-anime({
+var googleAnime = anime({
   targets: '.circle1',
   translateY: '30px',
   delay: function(el, index) {
